db84b49407e4f1ca5a8074d5444b480fcc935a22
Ryan Heisler <info@ryanheisler.com>
2022-03-08 18:08:09 -0500

    add reach UI to demo warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { AlertDialog, AlertDialogDescription, AlertDialogLabel } from "@reach/alert-dialog";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showDialog, setShowDialog] = React.useState(false);
+    const open = () => setShowDialog(true);
+    const close = () => setShowDialog(false);
+    const cancelRef = React.useRef<HTMLButtonElement>(null);
+    return (<div>
+        <button onClick={open}>Delete something</button>
+        {showDialog && (
+            <AlertDialog leastDestructiveRef={cancelRef}>
+                <AlertDialogLabel>Please Confirm!</AlertDialogLabel>
+                <AlertDialogDescription> Are you sure you want to delete something? This action is permanent, and
+                    we're totally not just flipping a field called "deleted" to "true" in our database, we're
+                    actually deleting something. </AlertDialogDescription>
+                <div className="alert-buttons">
+                    <button onClick={close}>Yes, delete</button>
+                    {" "}
+                    <button ref={cancelRef} onClick={close}>Nevermind, don't delete.</button>
+                </div>
+            </AlertDialog>)}    </div>);
 }
 
 export default App;
